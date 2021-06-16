e8eebc250c1d21efab364429bcb204d01a5732f4
Leonardo Ramirez <deiividramirez@gmail.com>
2021-06-16 14:10:24 -0500

    modified:   Ciclo 1 (Grupo 36)/Retos/Reto5.py

--- Ciclo 1 (Grupo 36)/Retos/Reto5.py
+++ Ciclo 1 (Grupo 36)/Retos/Reto5.py
@@ -3,7 +3,19 @@ import pandas as pd
 
 def examen(ruta_archivo_csv: str) -> dict:
      try:
-          return pd.read_csv(ruta_archivo_csv)
+          archivo = pd.read_csv( ruta_archivo_csv )
      except:
           print("No es posible leer los Datos, verifique la ruta")
+     
+     tabla = archivo.groupby("sexo").agg(np.mean)
+     
+     dic = archivo.to_dict()
+     
+     return f"""
+{tabla}
 
+{dic}
+"""
+
+
+print(examen('https://raw.githubusercontent.com/pepija/mintic-p36/main/examen_hemoglobina.csv'))

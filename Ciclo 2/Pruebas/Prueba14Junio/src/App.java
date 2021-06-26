a03ba496fce0b66605a2d493bf2da3dda66bd73f
Leonardo Ramirez <deiividramirez@gmail.com>
2021-06-26 15:20:54 -0500

    renamed:    Ciclo 2/Prueba14Junio/src/App.java -> Ciclo 2/Pruebas/Prueba14Junio/src/App.java

--- Ciclo 2/Pruebas/Prueba14Junio/src/App.java
+++ Ciclo 2/Pruebas/Prueba14Junio/src/App.java
@@ -2,6 +2,8 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
         var Holi = 34;
-        System.out.println("KHE " + Holi);
+        var a = 4;
+        var b = a == 3 ? 0 : 1;
+        System.out.println("KHE - " + a + " - " + b);
     }
 }

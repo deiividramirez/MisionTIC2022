737617a140a2d895befc44bf2b2745211c3fb8bb
Leonardo Ramirez <deiividramirez@gmail.com>
2021-07-08 12:19:42 -0500

    08 Julio

--- Ciclo 2 (Grupo 61)/Clases/3 Julio/Sistema de Notas/src/main/java/co/edu/utp/mistiontic2022/c2/Estudiante.java
+++ Ciclo 2 (Grupo 61)/Clases/3 Julio/Sistema de Notas/src/main/java/co/edu/utp/mistiontic2022/c2/Estudiante.java
@@ -1,17 +1,80 @@
 package co.edu.utp.mistiontic2022.c2;
 
 public class Estudiante {
-     private int codigo;
-     private String nombres;
-     private String apellidos;
-     private int semestreActual;
-     private String genero;
 
-     public Estudiante(int codigo, String nombres, String apellidos, int semestreActual, String genero) {
-          this.codigo = codigo;
-          this.nombres = nombres;
-          this.apellidos = apellidos;
-          this.semestreActual = semestreActual;
-          this.genero = genero;
-     }
+    //Atributos
+    private int codigo;
+    private String nombres;
+    private String apellidos;
+    private int semestreActual;   
+    private String genero;
+
+    //Constructores
+    public Estudiante(int pCodigo, String pNombres, String pApellidos, int pSemestreActual, String pGenero ){
+        this.codigo = pCodigo;
+        this.nombres = pNombres;
+        this.apellidos = pApellidos;
+        this.semestreActual = pSemestreActual;
+        this.genero = pGenero;
+    }
+
+    public Estudiante(){
+        this.codigo = 0;
+        this.nombres = "Nombres Sin Asignar";
+        this.apellidos = "Apellidos Sin Asignar";
+        this.semestreActual = 1;
+        this.genero = "Sin Asignar";
+    }
+
+    //Métodos
+    public void mostrarInfoEstudiante(){
+        System.out.println("-----Detalles estudiante----");
+        System.out.println("Código -> "+this.codigo);
+        System.out.println("Nombres -> "+this.nombres);
+        System.out.println("Apellidos -> "+this.apellidos);
+        System.out.println("Semestre Actual -> "+this.semestreActual);
+        System.out.println("Género -> "+this.genero);
+
+    }
+
+    //Getters
+    public int getCodigo() {
+        return codigo;
+    }
+    public String getNombres() {
+        return nombres;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public int getSemestreActual() {
+        return semestreActual;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    //Setters
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public void setNombres(String nombres) {
+        this.nombres = nombres;
+    }
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public void setSemestreActual(int semestreActual) {
+        this.semestreActual = semestreActual;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+    
 }
--- Ciclo 2 (Grupo 61)/Clases/3 Julio/Sistema de Notas/src/main/java/co/edu/utp/mistiontic2022/c2/Nota.java
+++ Ciclo 2 (Grupo 61)/Clases/3 Julio/Sistema de Notas/src/main/java/co/edu/utp/mistiontic2022/c2/Nota.java
@@ -1,76 +1,116 @@
 package co.edu.utp.mistiontic2022.c2;
 
 public class Nota {
-     // Atributos
-     private int escala100;
-     private double escala5;
-     private String cualitativa;
-     // private String nombre;
 
-     // Constructores
-     Nota() {
-          this.escala100 = 0;
-          this.escala5 = 0;
-          this.cualitativa = "F";
-     }
+    //1) Atributos (antes variables que estaban sueltas)
+    private int escala100;
+    private double escala5;
+    //private double atributoPruebaFirma;//Análisis de choque de firmas 
+    private String cualitativa;
+    private String nombre;//Evaluación, taller, o reto correspondiente
 
-     Nota(int pEscala100) {
-          this.escala100 = pEscala100;
-          this.escala5 = pEscala100 / 20;
-          if(pEscala100 >= 60){
-              this.cualitativa = "Aprobado";
-          }else{
-              this.cualitativa = "Reprobado";
-          }
-     }
+    //2) Constructor(es)
+    Nota(){
+        this.escala100 = 0;
+        this.escala5 = 0;
+        this.cualitativa = "";
+    }
 
-     Nota(double pEscala5) {
-          this.escala5 = pEscala5;
-          this.escala100 = (int)pEscala5 * 20;
-          if(pEscala5 >= 2.95){
-              this.cualitativa = "Aprobado";
-          }
-          else{
-              this.cualitativa = "Reprobado";
-          }
-     }
-     Nota(double pEscala5, int pEscala100, String pCualitativo) {
-          this.escala5 = pEscala5;
-          this.escala100 = pEscala100;
-          this.cualitativa = pCualitativo;
-      }
+    Nota(int pEscala100){
+        this.escala100 = pEscala100;
+        this.escala5 = (double)pEscala100 / 20;
+        if(pEscala100 >= 60){
+            this.cualitativa = "Aprobado";
+        }else{
+            this.cualitativa = "Reprobado";
+        }
+    }
 
-      // Funciones
-     public void mostrarNotaConsola() {
-          System.out.println("-----InfoNota-------");
-          System.out.println("Valor Escala 5 -> " + this.escala5);
-          System.out.println("Valor Escala 100 -> " + this.escala100);
-          System.out.println("Cualitativa  -> " + this.cualitativa);
-     }
+    Nota(double pEscala5){
+        this.escala5 = pEscala5;
+        this.escala100 = (int)pEscala5 * 20;
+        if(pEscala5 >= 2.95){
+            this.cualitativa = "Aprobado";
+        }else{
+            this.cualitativa = "Reprobado";
+        }
+    }
 
-     // Getter (Quienes devuelven el valor de una variable privada)
-     public String getCualitativa() {
-          return cualitativa;
-     }
+    // //Alternativa con choque de firmas, un constructor que generaliza
+    // Nota(int a, int b, int modo){
 
-     public int getEscala100() {
-          return escala100;
-     }
-  
-     public double getEscala5() {
-          return escala5;
-     }
+    //     if(modo == 1){
 
-     // Setter (Quienes cambian el valor de una variable)
-     public void setCualitativa(String cualitativa) {
-          this.cualitativa = cualitativa;
-     }
-  
-     public void setEscala100(int escala100) {
-          this.escala100 = escala100;
-     }
-  
-     public void setEscala5(double escala5) {
-          this.escala5 = escala5;
-     }
+    //     }else if(modo == 2){
+
+    //     }else if(modo == 3){
+
+    //     }else{
+
+    //     }
+
+    // }
+
+    Nota(double pEscala5, int pEscala100, String pCualitativo){
+        this.escala5 = pEscala5;
+        this.escala100 = pEscala100;
+        this.cualitativa = pCualitativo;
+
+    }
+    
+    //3) Métodos -> comportamiento
+    //Forma específica de mostrar la información
+    //Conversión de las notas
+    public void mostrarNotaConsola(){
+        System.out.println("-----InfoNota-------");
+        System.out.println("Valor Escala 5 -> " + this.escala5);
+        System.out.println("Valor Escala 100 -> " + this.escala100);
+        System.out.println("Cualitativa  -> " + this.cualitativa);
+    }
+
+    //Conversión de notas que fueron creadas vacías y actualizadas parcialmente
+    public void convertirNota5_100(){
+        this.escala100 = (int)(this.escala5 * 20);
+    }
+
+    public void convertirNota100_5(){
+        this.escala5 = (double)this.escala100/20;
+    }
+
+    public void convertirNota100_Cualitativo(){
+        if(this.escala100 >= 60){
+            this.cualitativa = "Aprobado";
+        }else{
+            this.cualitativa = "Reprobado";
+        }
+    }
+
+    
+    //4) Getters
+    public String getCualitativa() {
+        return cualitativa;
+    }
+
+    public int getEscala100() {
+        return escala100;
+    }
+
+    public double getEscala5() {
+        return escala5;
+    }   
+
+    //5) Setters
+    public void setCualitativa(String cualitativa) {
+        this.cualitativa = cualitativa;
+    }
+
+    public void setEscala100(int escala100) {
+        this.escala100 = escala100;
+    }
+
+    public void setEscala5(double escala5) {
+        this.escala5 = escala5;
+    }
+
+
 }
